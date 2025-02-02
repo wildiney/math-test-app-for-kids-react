fb3c01bdaa27d3ff530f8c448407df2f5cbc18de
Wildiney Di Masi <wildiney@gmail.com>
2025-02-02 13:23:38 -0300

    Updates

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/math-test-app-for-kids-react/sw.js', { scope: '/math-test-app-for-kids-react/' })})}
