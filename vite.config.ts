1578624116f0e19f93db5e092f96b62a81657ac1
Wildiney Di Masi <wildiney@gmail.com>
2025-02-02 13:23:25 -0300

    update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -5,36 +5,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      },
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'MathGame',
-        short_name: 'MathGame',
-        description: 'A simple Math App Game',
-        theme_color: '#134e4a',
-        icons: [
-          {
-            src: 'logo192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'logo512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })],
-  base: '/math-test-app-for-kids-react',
-  server: {
-    open: true,
-    port: 3000
-  }
+    react()
+  ]
 })
